75d90ae8abddcb6f20e5dca0ca4bfe97041f5364
Oriana <oriana.nohemi22@gmail.com>
2020-12-27 11:51:05 -0500

    Ejercicio realizado con condicionales

--- conversor.py
+++ conversor.py
@@ -1,8 +1,42 @@
-coin = float(input("¿Cuántos pesos colombianos tienes?"))
+menu = """
+  Bienvenido al conversor de monedas
 
-dollar_value = 3500
+  1 - Pesos colombianos
+  2 - Pesos argentinos
+  3 - Pesos mexicanos
 
-dollars = round(coin / dollar_value)
-dollars = str(dollars)
+  Elige una opcion:
 
-print('Tienes $' + dollars + 'dólares')
+"""
+
+option = float(input(menu))
+
+if option == 1:
+    coin = float(input("¿Cuántos pesos colombianos tienes?"))
+
+    dollar_value = 3500
+
+    dollars = round(coin / dollar_value)
+    dollars = str(dollars)
+
+    print('Tienes $' + dollars + 'dólares')
+elif option == 2:
+    coin = float(input("¿Cuántos pesos argentinos tienes?"))
+
+    dollar_value = 65
+
+    dollars = round(coin / dollar_value)
+    dollars = str(dollars)
+
+    print('Tienes $' + dollars + 'dólares')
+elif option == 3:
+    coin = float(input("¿Cuántos pesos mexicanos tienes?"))
+
+    dollar_value = 24
+
+    dollars = round(coin / dollar_value)
+    dollars = str(dollars)
+
+    print('Tienes $' + dollars + 'dólares')
+else:
+    print('Ingresa una opción valida, por favor')
