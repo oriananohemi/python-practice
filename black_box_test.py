ed1647baf1d360ba236b0cad9c12aa27d7de6db1
Oriana <oriana.nohemi22@gmail.com>
2021-01-01 13:21:01 -0500

    Ejercicio de test

---
+++ black_box_test.py
@@ -0,0 +1,28 @@
+import unittest
+
+
+def sum(element1, element2):
+    return element1 + element2
+
+
+class blackBoxTest(unittest.TestCase):
+
+    def test_sum(self):
+        num_1 = 10
+        num_2 = 5
+
+        answer = sum(num_1, num_2)
+
+        self.assertEqual(answer, 15)
+
+    def test_sum_negative(self):
+        num_1 = -10
+        num_2 = -7
+
+        answer = sum(num_1, num_2)
+
+        self.assertEqual(answer, -17)
+
+
+if __name__ == '__main__':
+    unittest.main()
